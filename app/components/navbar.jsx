8994d6dcf982f65633cccc67fd7108015e03a06d
Saumya Shahi <saumyashahi05@gmail.com>
2024-02-29 21:48:01 +0530

    Commited Changes in Components and Sections

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,10 +1,28 @@
-export default function NavBar() {
-    return (
-        <div className="bg-red">
-            <h1>NavBar</h1>
-            <p>
-                This is the navbar.
-            </p>
+import React from 'react';
+import './components/Navbar.css';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <a href="/">Your Logo</a>
         </div>
-    );
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <a href="/" className="nav-links">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="/about" className="nav-links">About</a>
+          </li>
+          <li className="nav-item">
+            <a href="/services" className="nav-links">Services</a>
+          </li>
+          <li className="nav-item">
+            <a href="/contact" className="nav-links">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
