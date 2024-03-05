827aab83e37cac4cf0a105abba90fec7cbe97a32
RamKishore <mramki1803@gmail.com>
2024-03-05 15:14:24 +0530

    Updated

--- app/components/Carousel.jsx
+++ app/components/Carousel.jsx
@@ -6,6 +6,9 @@ import "../styles/style.css";
 import Image from "next/image";
 import btnleft from "../../public/Btn-left.svg";
 import btnright from "../../public/Btn-right.svg";
+import leftcomma from "../../public/left-comma.svg";
+import rightcomma from "../../public/right-comma.svg";
+
 const customImageStyle = {
   filter: "brightness(100%)", // Adjust brightness as needed
   opacity: 1, // Set opacity to 1 for full visibility
@@ -121,46 +124,56 @@ export default function Slider2() {
                 </div>
               </div>
             </div>
+
             <div className="flex flex-col ml-5 w-[63%] max-md:ml-0 max-md:w-full">
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/9cca5fd751af3f5aef884b1815f011426b283ad1bfed9e3f37a0e040c1083bf6?apiKey=09d58e4ed20e4986bcbec4c048901f7f&"
-                className="self-end w-3.5 aspect-[0.58]"
-              />
-              <div className="text-base leading-9 text-slate-500 max-md:mt-5 max-md:max-w-full">
-                IIIT Kottayam has demonstrated rapid progress in its four years
-                of existence. Having managed to set up a permanent campus
-                despite the terrible weather conditions and the back-to-back
-                floods, is testament to the resileience of our faculty. One
-                among the other innovations is a fully functioning student run
-                mess committee which manages to serve 300 students 3 meals a day
-                along with snacks. Moving higher on Maslow’s hierarchy of needs,
-                our students’ need for intellectual stimulation is readily
-                satisfied by a dynamically changing syllabus sensitive to the
-                developments of the industry. We wish to install the temperament
-                of research in our students and thus have architected a syllabus
-                focusing on emerging fields in data science and artificial
-                intelligence. To give our students an understanding of how
-                management of funds and people takes place in the real world we
-                have also decided to introduce single credit business courses
-                into our syllabus. The combination of the two disciplines gives
-                our students an all-roundedperspective of the industry’s
-                challenges and prepares them to take on any situation head- on.
-                We look forward to working with your organization and
-                <br />
-                welcome you to the on-campus placements of the batch 2O23 as-on
-                campus recruiters.
-                <br />
-                With Best Wishes!
+              <div className="flex">
+                <Image
+                  loading="lazy"
+                  src={leftcomma}
+                  height={40}
+                  width={40}
+                  className="self-start aspect-[0.58] max-md:mt-10"
+                />
+
+                <div className="text-base leading-9 text-slate-500 max-md:mt-5 max-md:max-w-full">
+                  IIIT Kottayam has demonstrated rapid progress in its four
+                  years of existence. Having managed to set up a permanent
+                  campus despite the terrible weather conditions and the
+                  back-to-back floods, is testament to the resileience of our
+                  faculty. One among the other innovations is a fully
+                  functioning student run mess committee which manages to serve
+                  300 students 3 meals a day along with snacks. Moving higher on
+                  Maslow’s hierarchy of needs, our students’ need for
+                  intellectual stimulation is readily satisfied by a dynamically
+                  changing syllabus sensitive to the developments of the
+                  industry. We wish to install the temperament of research in
+                  our students and thus have architected a syllabus focusing on
+                  emerging fields in data science and artificial intelligence.
+                  To give our students an understanding of how management of
+                  funds and people takes place in the real world we have also
+                  decided to introduce single credit business courses into our
+                  syllabus. The combination of the two disciplines gives our
+                  students an all-roundedperspective of the industry’s
+                  challenges and prepares them to take on any situation head-
+                  on. We look forward to working with your organization and
+                  <br />
+                  welcome you to the on-campus placements of the batch 2O23
+                  as-on campus recruiters.
+                  <br />
+                  With Best Wishes!
+                </div>
               </div>
+
+              <Image
+                loading="lazy"
+                src={rightcomma}
+                height={40}
+                width={40}
+                className="self-end aspect-[0.58] max-md:mt-10"
+              />
             </div>
             <Image src={btnright} alt="right-button" layout="intrinsic" />
           </div>
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/6ba44758c756b37e60df1e8cccb8b7169a858d0621a9d909bb240653b8ef2337?apiKey=09d58e4ed20e4986bcbec4c048901f7f&"
-            className="self-end mt-96 w-3.5 aspect-[0.58] max-md:mt-10"
-          />
         </div>
       </div>
     </div>,
