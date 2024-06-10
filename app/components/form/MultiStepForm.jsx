99285e63322d8fd8fe02c27cfc6e4111e5475b5b
Aroson <74296409+Aroson1@users.noreply.github.com>
2024-06-10 17:35:19 +0000

    Fixed vercel depolyment

--- app/components/form/MultiStepForm.jsx
+++ app/components/form/MultiStepForm.jsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React, { useState } from "react";
-import Step1 from "./Step1";
-import Step2 from "./Step2";
-import Step3 from "./Step3";
+import Step1 from "./step1";
+import Step2 from "./step2";
+import Step3 from "./step3";
 import StepIndicator from "./StepIndicator";
 import "./MultiStepForm.css";
 
