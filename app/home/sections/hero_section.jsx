869fcba81163e2dea218b09261d64e5310c97537
Aroson <74296409+Aroson1@users.noreply.github.com>
2024-07-11 13:13:05 +0530

    Update hero_section.jsx

--- app/home/sections/hero_section.jsx
+++ app/home/sections/hero_section.jsx
@@ -33,7 +33,7 @@ export default function Hero() {
             >
               C
             </span>
-            OMMITEE
+            OMMITTEE
             <div
               style={{
                 background:
